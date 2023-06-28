7b4d50d825b558e95a0d5f964609cdbd0b560355
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2023-06-27 21:03:19 -0300

    On Fire bb

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
@@ -12,24 +13,16 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      {/* titulo */}
       <h1>GifExpertApp</h1>
 
-      {/* input */}
       <AddCategory
         // setCategories={setCategories}
         onNewCategory={(event) => onAddCategories(event)}
       />
 
-      {/* listado de Gif */}
-
-      {/* <button onClick={onAddCategories}>Agregar</button> */}
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
-      {/* Gif Item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
