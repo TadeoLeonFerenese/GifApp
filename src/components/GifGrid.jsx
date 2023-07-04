ecfcbb5daf37cbcf6a7d8bd8cfc3707f7fd207a9
Tadeo <teoleon2013@gmail.com>
2023-07-04 12:40:28 -0300

    On fire BB

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,29 @@
-import { useEffect } from "react"; // useEffect se usa para disparar un efecto secundario cuando el componente se renderiza pro primera vez
+import { useEffect, useState } from "react"; // useEffect se usa para disparar un efecto secundario cuando el componente se renderiza pro primera vez
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
   useEffect(() => {
-    getGifs(category);
+    //en useEffect no se puede usar funciones asyncronas por eso se remplaza entre una de las tantas con ".then"
+    getImages();
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {/* Este trozo de codigo lo que hace es traerme los titulos de forma
+        dinamica mapeando primero la data */}
+        {images.map(({ title, id }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
